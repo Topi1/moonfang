b16c69fbea2976e9d918be448e8d5a919f8fd942
Topi Järvinen <topi.jarvinen1@gmail.com>
2023-07-05 17:16:43 +0300

    1.47

--- components/Homepage.jsx
+++ components/Homepage.jsx
@@ -7,46 +7,50 @@ export default function Homepage() {
   
   
     return (
-        <div className='maincon'>
+        <div className='homemain'>
+            <div className="homeside1"></div>
+            <div className='maincon'>
 
-        <section className='con'>
-            <section className="headers">
-                    <h1>GAMING NEWS</h1>
-                    <a href=""><h2>More News</h2></a>
-            </section>
-            <a className="news">
-                
-                <section>
-                    <img src={mindflayer} alt="" className='articlepic'/>
+            <section className='con'>
+                <section className="headers">
+                        <h1>GAMING NEWS</h1>
+                        <a href=""><h2>More News &#8680;</h2></a>
                 </section>
-                <section className ="textsection">
-                <div className='textdivide1'>
-                    <h2>BALDUR'S GATE III</h2>
-                </div>
-                <div className='textdivide2'>
-                    <h1>Baldur’s Gate III will launch four weeks early on PC, delayed on PS5</h1>
-                </div>
-                </section>   
-            </a>
-        </section>
-        
-        <section className='con'>
-            <h1>GAMES</h1>
-            <section className="news">
+                <a className="news">
+                    
+                    <section>
+                        <img src={mindflayer} alt="" className='articlepic'/>
+                    </section>
+                    <section className ="textsection">
+                    <div className='textdivide1'>
+                        <h2>BALDUR'S GATE III</h2>
+                    </div>
+                    <div className='textdivide2'>
+                        <h1>Baldur’s Gate III will launch four weeks early on PC, delayed on PS5</h1>
+                    </div>
+                    </section>   
+                </a>
             </section>
-        </section>
+            
+            <section className='con'>
+                <h1>GAMES</h1>
+                <section className="news">
+                </section>
+            </section>
 
-        <section className='con'>
-            <section className="headers">
-                <h1>NEWSLETTER</h1>
+            <section className='con'>
+                <section className="headers">
+                    <h1>NEWSLETTER</h1>
+                </section>
+                <section className="news">
+                </section>
             </section>
-            <section className="news">
-            </section>
-        </section>
 
-        <div></div>
-        
-        
+            <div></div>
+            
+            
+        </div>
+        <div className="homeside2"></div>
     </div>
   )
 }
