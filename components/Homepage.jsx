f5a033e74f12351076bc88d73b103ecfbcbe9f34
Topi Järvinen <topi.jarvinen1@gmail.com>
2023-07-05 19:52:23 +0300

    1.48 module test

--- components/Homepage.jsx
+++ components/Homepage.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import "./Homepage.css"
+import css from "./Homepage.module.css"
 
 import mindflayer from "../pictures2/mindflayer.jpg"
 
@@ -7,42 +7,42 @@ export default function Homepage() {
   
   
     return (
-        <div className='homemain'>
-            <div className="homeside1"></div>
-            <div className='maincon'>
+        <div className={css.homemain}>
+            <div className={css.homeside1}></div>
+            <div className={css.maincon}>
 
-            <section className='con'>
-                <section className="headers">
+            <section className={css.con}>
+                <section className={css.headers}>
                         <h1>GAMING NEWS</h1>
                         <a href=""><h2>More News &#8680;</h2></a>
                 </section>
-                <a className="news">
+                <a className={css.news}>
                     
                     <section>
-                        <img src={mindflayer} alt="" className='articlepic'/>
+                        <img src={mindflayer} alt="" className={css.articlepic}/>
                     </section>
-                    <section className ="textsection">
-                    <div className='textdivide1'>
+                    <section className ={css.textsection}>
+                    <div className={css.textdivide1}>
                         <h2>BALDUR'S GATE III</h2>
                     </div>
-                    <div className='textdivide2'>
+                    <div className={css.textdivide2}>
                         <h1>Baldur’s Gate III will launch four weeks early on PC, delayed on PS5</h1>
                     </div>
                     </section>   
                 </a>
             </section>
             
-            <section className='con'>
+            <section className={css.con}>
                 <h1>GAMES</h1>
-                <section className="news">
+                <section className={css.news}>
                 </section>
             </section>
 
-            <section className='con'>
-                <section className="headers">
+            <section className={css.con}>
+                <section className={css.headers}>
                     <h1>NEWSLETTER</h1>
                 </section>
-                <section className="news">
+                <section className={css.news}>
                 </section>
             </section>
 
@@ -50,7 +50,7 @@ export default function Homepage() {
             
             
         </div>
-        <div className="homeside2"></div>
+        <div className={css.homeside2}></div>
     </div>
   )
 }
